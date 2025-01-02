edf69a40d6c3b11c872cd026a47c9d87e586aadf
lnwchok <sitthichok.punlabpho@gmail.com>
2025-01-03 00:29:40 +0700

    Modify main.py

--- main.py
+++ main.py
@@ -4,30 +4,26 @@ from dotenv import load_dotenv
 
 import pandas as pd
 
-# from openpyxl import load_workbook
-
 load_dotenv()
 
 # ENV file (.env)
-FOLDER = "D:\OneDrive - MPS\BMC\Temp"
-FILENAME = "OUTPUT_MH_SUM_update_2024.xlsm"
-SHEET_NAME = "DataBase1"
 
-folder_path = FOLDER or os.getenv("FOLDER")
-xlsx_filename = FILENAME or os.getenv("FILENAME")
-sheet_name = SHEET_NAME or os.getenv("SHEET_NAME")
+folder_path = os.getenv("FOLDER") #or FOLDER
+xlsx_filename = os.getenv("FILENAME") #or FILENAME
+sheet_name = os.getenv("SHEET_NAME") #or SHEET_NAME
 
-filename = f"{folder_path}\{xlsx_filename}"
+filename = f"{folder_path}\\{xlsx_filename}"
 
-job_no = sys.argv[1:][0] or ""
+job_no = sys.argv[1:][0]
 
 data = pd.read_excel(
     filename,
     sheet_name=sheet_name,
     usecols="A:B, E:F, I",
-    converters={"YEAR": str, "MONTH": str},
-).query("YEAR == 2024" and "`JOB NO.` == @job_no")
+    converters={"YEAR": int, "MONTH": int},
+).query("YEAR == 2024").query("`JOB NO.` == @job_no")
 
+# ).query("`JOB NO.` == @job_no")
 # data1 = data.query("YEAR == '2024'")
 
 print(data)
